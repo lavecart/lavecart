88ce3ac030f4e4de22e2bfb37df857635c090546
nafeeur10 <nafeeur.kuet.cse6@gmail.com>
2021-09-04 23:07:01 +0600

    Swagger added and Registration, Login done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,73 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    /** @OA\Info(title="API Documentation for Lavecart", version="1.1") */
+
+    /**
+     * @OA\Post(
+     *     path="api/register",
+     *     description="Registration of an user",
+     *     tags={"Authentication"},
+     *     @OA\RequestBody(
+     *           @OA\MediaType(mediaType="multipart/form-data",
+     *              @OA\Schema(
+     *                  @OA\Property(property="name", type="String"),
+     *                  @OA\Property(property="emaili", type="String"),
+     *                  @OA\Property(property="password", type="String")
+     *             )
+     *         )
+     *      ),
+     *     @OA\Response(response="201", description="Registration Successful"),
+     *     @OA\Response(response="400", description="Registration Unsuccessful")
+     * )
+    */
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+            'token_type'   => 'Bearer'
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function me(Request $request)
+    {
+        return $request->user();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\StoreFrontController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+/** Authentication Routes */
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
