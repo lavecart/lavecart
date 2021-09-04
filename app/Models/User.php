9257b5a357fd6e2e426aa2ad62f3aea5f1e9b907
nafeeur10 <nafeeur.kuet.cse6@gmail.com>
2021-09-04 21:19:56 +0600

    Token Manager and Auth Controller created

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,9 +24,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for arrays.
