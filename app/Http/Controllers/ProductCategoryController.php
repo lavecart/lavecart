f6bc25740e16655f672b3ac1db8bce4c836ff55c
hafizurcsejnu <hafizur.csejnu.com>
2021-12-04 18:13:55 +0600

    Category Migration Model and Controller Created

---
+++ app/Http/Controllers/ProductCategoryController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductCategory;
+use Illuminate\Http\Request;
+
+class ProductCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProductCategory  $productCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductCategory $productCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProductCategory  $productCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductCategory $productCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProductCategory  $productCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductCategory $productCategory)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_12_04_071358_create_product_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false)->index();
+            $table->integer('parent_id')->default(0);
+            $table->text('description')->nullable();
+            $table->string('icon')->nullable();
+            $table->boolean('active')->default(true);
+            $table->integer('sort_order')->nullable();
+            $table->string('slug')->default();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_categories');
+    }
+}
